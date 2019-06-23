1a64d1bf9d94a3758f679a19f0a8d4233bfb7092
ruby86123 <ruby86123@gmail.com>
2019-06-23 15:59:06 +0800

    06/23
    phase1 with global router and local router

--- src/component.cpp
+++ src/component.cpp
@@ -1,3 +1,22 @@
+#include <fstream>
+#include <iostream>
 #include "component.h"
+
+void Net::calculateTDM(){
+    _TDM = 0;
+    for(unsigned int i=0; i<_cur_route.size();i++){
+        _TDM += lookupTable[_cur_route[i]->getCongestion()]; //check lookuptable name
+    }
+}
+void NetGroup::calculateTDM(){
+    _TDM = 0;
+    for(unsigned int i=0; i<_nets.size();i++){
+        _TDM += _nets[i]->getTDM();
+    }
+
+}
+void Edge::updateWeight(int iteration){
+    _weight = (_weight*iteration + _congestion)/(iteration+1);
+}
 
 
